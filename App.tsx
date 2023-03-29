ebdc0253b7cc204058092a6493aed885292c8d68
SebastianMayorgaCalderon <sebasmayorga7@gmail.com>
2023-03-28 18:05:43 -0600

    [Added basic routing to app root]

--- App.tsx
+++ App.tsx
@@ -6,20 +6,41 @@
  * @flow strict-local
  */
 
+import {NavigationContainer} from '@react-navigation/native'
+
 import React from 'react';
 import type {Node} from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
 
 import PokedexApp from './src';
 import {Provider} from 'react-redux';
 import store from './src/state';
 
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+
 const App: () => Node = () => {
   return (
     <Provider store={store}>
-      <SafeAreaView>
-        <PokedexApp />
-      </SafeAreaView>
+      <NavigationContainer>
+        {/* <SafeAreaView>
+          <PokedexApp />
+        </SafeAreaView> */}
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    
     </Provider>
   );
 };
